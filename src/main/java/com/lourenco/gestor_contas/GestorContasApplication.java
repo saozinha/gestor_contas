08fa3357c5ef6faf6ba28418badd78cb8082c47d
Conceicao Lourenco <conceicaolourenzo@gmail.com>
2021-06-16 23:12:48 -0300

    first commit

---
+++ src/main/java/com/lourenco/gestor_contas/GestorContasApplication.java
@@ -0,0 +1,13 @@
+package com.lourenco.gestor_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lourenco/gestor_contas/GestorContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lourenco.gestor_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

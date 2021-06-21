76468d5a566af0efa9564a4f07745096cd0500d3
Conceicao Lourenco <conceicaolourenzo@gmail.com>
2021-06-21 00:34:02 -0300

    ajustes em  transferencia entre contas

--- src/main/java/com/lourenco/gestor_contas/inputOutPut/TransferInput.java
+++ src/main/java/com/lourenco/gestor_contas/inputOutPut/TransferInput.java
@@ -1,10 +1,12 @@
 package com.lourenco.gestor_contas.inputOutPut;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@ApiModel(value = "transfer", description = "Dados necessários para transferir valores")
 public class TransFerInput {
     private String numberAccount;
     private String CpfPersonReceiver;
